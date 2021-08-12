75b1e8f6424ab6b532e078db011de788878576c6
Cyebukayire <peacecyebukayire@gmail.com>
2021-08-12 13:46:59 +0200

    Add Repository

--- src/main/java/net/javaguides/entity/Product.java
+++ src/main/java/net/javaguides/entity/Product.java
@@ -1,5 +1,58 @@
 package net.javaguides.entity;
 
-public class Product {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
+@Entity
+@Table( name = "PRODUCT_TBL")
+public class Product {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	private String name;
+	private int quantity;
+	private double price;
+	
+	public Product() {
+		
+	}
+	
+	public Product(String name, int quantity, double price) {
+		super();
+		this.name = name;
+		this.quantity = quantity;
+		this.price = price;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	
 }
---
+++ src/main/java/net/javaguides/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package net.javaguides.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import net.javaguides.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer>{
+
+}
