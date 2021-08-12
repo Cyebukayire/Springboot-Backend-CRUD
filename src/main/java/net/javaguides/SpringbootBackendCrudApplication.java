4db94bb8006d8e2c3c6a3c60e5c83c07cbada447
Cyebukayire <peacecyebukayire@gmail.com>
2021-08-12 12:41:15 +0200

    first commit

---
+++ src/main/java/net/javaguides/SpringbootBackendCrudApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootBackendCrudApplication.class, args);
+	}
+
+}
