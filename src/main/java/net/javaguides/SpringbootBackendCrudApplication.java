e4b1641bcab87960688195275ce53e75467851d2
Cyebukayire <peacecyebukayire@gmail.com>
2021-08-12 14:38:23 +0200

    Add Product-Controllers

--- src/main/java/net/javaguides/SpringbootBackendCrudApplication.java
+++ src/main/java/net/javaguides/SpringbootBackendCrudApplication.java
@@ -1,10 +1,11 @@
 package net.javaguides;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootBackendCrudApplication {
+public abstract class SpringbootBackendCrudApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootBackendCrudApplication.class, args);
